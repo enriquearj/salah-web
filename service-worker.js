37ee7a26dadfbf90a61215a6a575ced5183b9467
enriquearj <84105227+enriquearj@users.noreply.github.com>
2025-09-02 09:43:43 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "selah-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+  // Agrega otros assets si los necesitas
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
